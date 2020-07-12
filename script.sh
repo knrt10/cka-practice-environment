8c0cfa3495c7010ba8ebd0b3e82a7d98d157c41f
knrt10 <tripathi.kautilya@gmail.com>
2020-07-12 17:26:15 +0530

    Fix minikube profile name for copying data to keys
    It is not fixed that user will always run minikube cluster as default as
    specified in readme. They can do it like `minikube start -p
    test-cluster`.
    
    Signed-off-by: knrt10 <tripathi.kautilya@gmail.com>

--- script.sh
+++ script.sh
@@ -3,9 +3,12 @@
 USERNAME=`id -un`
 cd ~/.kube
 mkdir -p keys
+
+profile_name=$(minikube profile list -o json | jq -r '.valid | .[] | select(.Status=="Running") | .Name')
+
 cp /Users/${USERNAME}/.minikube/ca.crt keys/
-cp /Users/${USERNAME}/.minikube/profiles/minikube/client.crt keys/
-cp /Users/${USERNAME}/.minikube/profiles/minikube/client.key keys/
+cp /Users/${USERNAME}/.minikube/profiles/$profile_name/client.crt keys/
+cp /Users/${USERNAME}/.minikube/profiles/$profile_name/client.key keys/
 
 gsed -i "s/\/Users\/${USERNAME}\/.minikube\/profiles\/minikube/.\/keys/g" config
 gsed -i "s/\/Users\/${USERNAME}\/.minikube/.\/keys/g" config
